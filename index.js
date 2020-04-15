88af50af34de77aedcc4916e750625db59618ff0
Raman Liaukovich <2038634@gmail.com>
2020-04-15 17:01:02 +0300

    feat: realize mode switching

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ document.querySelector('.nav-bar').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('.menu').classList.toggle('active');
 });
+
+
+function uncheck() {
+    document.getElementById('checkbox').checked = false;
+}
